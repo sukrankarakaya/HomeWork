e637ab0d6f0cc4f7d21b1e8f982a0014ad16e54f
Şükran Karakaya <sukran.karakaya@ogrenci.selcuk.edu.tr>
2021-05-04 05:06:01 +0300

    Mernis sistemi

---
+++ MernisTest/src/tr/gov/nvi/tckimlik/WS/Main.java
@@ -0,0 +1,24 @@
+package tr.gov.nvi.tckimlik.WS;
+
+import java.rmi.RemoteException;
+
+public class Main {
+
+	public static void main(String[] args) throws RemoteException {
+		KPSPublicSoapProxy kpsPublic =new KPSPublicSoapProxy();
+				
+				boolean result =kpsPublic.TCKimlikNoDogrula(
+						Long.parseLong("25459156412"),
+						"ÞÜKRAN","KARAKAYA",
+						2001
+						
+						);
+
+		System.out.println("dogrulama : "+ (result ? "Baþarýlý":"Baþarýsýz"));
+	}
+
+	
+
+	
+
+}
