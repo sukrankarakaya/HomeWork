470201fdf9dc433dfefa00ffe35a8f97957946de
Şükran Karakaya <sukran.karakaya@ogrenci.selcuk.edu.tr>
2021-05-08 16:58:09 +0300

    .

---
+++ inheritance/src/inheritance/Main.java
@@ -0,0 +1,24 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		IndividualCustomer engin = new IndividualCustomer();
+
+		engin.customerNumber="12345";
+		
+		
+		CorporateCustomer hepsiBurada= new  CorporateCustomer();
+		
+		hepsiBurada.customerNumber="45665";
+		
+		CustomerManager customerManager = new CustomerManager()	;
+		
+		Customer[] customers= {engin, hepsiBurada};
+		
+		customerManager.addMultiple(customers);
+
+	}
+
+}
